303a06e9e2c3b3696eb2a45ab04e6724d84dc048
ben3amer <mohamedaminbnamer@gmail.com>
2023-05-02 00:01:52 +0100

    Added addUser interface.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Login  from './pages/login';
 import Vehicules from './pages/vehicules';
 import Users from './pages/users';
+import AddUser from './pages/users/add';
 import {
   BrowserRouter as Router,
   Routes,
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Login/>} />
           <Route path="/users" element={<Users/>}/>
+          <Route path="/users/add" element={<AddUser/>}/>
           <Route path="/vehicules" element={<Vehicules/>}/>
         </Routes>
       </Router>
